cb4b26d61fd20817953e02107c5cebcbd39292fc
rehanmirza76 <abdulrehan0317@gmail.com>
2024-05-19 16:46:30 +0500

    Added Hamburger

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 
+function showSidebar() {
+    const sideBar = document.querySelector("#navbar");
+    sideBar.style.display = "flex";
+
+  }
+  
+  function hideSidebar() {
+    const sideBar = document.querySelector("#navbar");
+    sideBar.style.display = "none";
+  }
+  
+  document.addEventListener("DOMContentLoaded", () => {
+    document.querySelector("#burger").onclick = showSidebar;
+  });
+  document.addEventListener("DOMContentLoaded", () => {
+    document.querySelector("#cross").onclick = hideSidebar;
+  });
+ 
