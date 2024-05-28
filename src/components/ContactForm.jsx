8367712fe8ea2ac7206b6661d732fec634a7bb40
rehanmirza76 <abdulrehan0317@gmail.com>
2024-05-28 16:50:32 +0500

    Added Contact Form

---
+++ src/components/ContactForm.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+
+const ContactForm = () => {
+  return (
+    <div className="text-left flex justify-center py-5 bg-[#202020]">
+      <form
+        action=""
+        className="flex text-white flex-col  p-5 space-y-2 md:space-y-5 md:w-[50vw] w-[100vw]"
+      >
+        <div className="item">
+          <label>Your Name:</label>
+          <input
+            type="text"
+            placeholder="Enter Your Name "
+            className="bg-black  px-2 py-2 w-[100%] rounded-sm"
+          />
+        </div>
+        <div className="item">
+          <label>Email:</label>
+          <input
+            type="text"
+            placeholder="Enter Your Email "
+            className="bg-black
+  px-2 py-2 w-[100%] rounded-sm"
+          />
+        </div>
+        <div className="item">
+          <label>Subject:</label>
+          <input
+            type="text"
+            placeholder="Enter Subject "
+            className="bg-black
+  px-2 py-2 w-[100%] rounded-sm"
+          />
+        </div>
+        <div className="item">
+          <label>Message:</label>
+          <textarea
+            rows="6"
+            placeholder="Your Message Here "
+            className="bg-black  px-2 py-2 w-[100%] rounded-sm"
+          ></textarea>
+        </div>
+        <button className=" btn-light hover:bg-[#535353]">SUBMIT</button>
+      </form>
+    </div>
+  );
+};
+
+export default ContactForm;
--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -3,11 +3,13 @@ import Navigation from "../components/Navigation";
 import Footer from "../components/Footer";
 import Hero2 from "../components/Hero2";
 import FooterBar from "../components/FooterBar";
+import ContactForm from "../components/ContactForm";
 
 const contact = () => {
   return <div>
           <Navigation />
           <Hero2 heading="CONTACT US." para="Feel free to reach out for collaboration, job opportunities, or any queries"/>
+          <ContactForm/>
           <Footer/>
           <FooterBar/>
   </div>;
