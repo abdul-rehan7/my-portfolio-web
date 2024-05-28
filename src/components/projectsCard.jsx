ee9c47ade7b92f3d73c598337480511f5b8f6cff
rehanmirza76 <abdulrehan0317@gmail.com>
2024-05-28 20:16:37 +0500

    Corrected Links

--- src/components/projectsCard.jsx
+++ src/components/projectsCard.jsx
@@ -7,68 +7,63 @@ const projectsCard = () => {
       <div className="container  flex flex-col md:flex-row md:space-y-0 items-center justify-center space-y-5 md:space-x-5 ">
         {/* CARD  */}
 
-        <div className="card space-y-2 md:w-[25%] w-[90vw] h-auto bg-[#1b1b1b] p-2 rounded-sm"   data-aos="zoom-in">
+        <div className="card space-y-2 md:w-[25%] w-[90vw] h-[60vh] bg-[#1b1b1b] p-2 rounded-sm"   data-aos="zoom-in">
           <div className="items space-y-2 ">
             <img src="/imgs/projs/1.png" alt="" />
-            <h1 className="font-semibold">Archiods</h1>
-            <p className="text-sm font-normal text-center">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt nisi
-              fuga provident. Magni, aperiam! Minima. Lorem ipsum dolor sit amet
-              consectetur adipisicing elit. Velit, omnis!
+            <h1 className="font-semibold">Microsoft Clone</h1>
+            <p className="text-sm font-normal text-left h-[17vh]">
+            I built a Microsoft clone that mimics the core functionalities of Office applications, enabling document creation, editing, and cloud storage integration.
             </p>
           </div>
           <div className="buttons flex justify-evenly">
             <a
               className="btn  w-[40%]"
-              href="https://www.google.com/search?q=React&rlz"
+              href="https://mr76-microsoft-clone.vercel.app/"
               target="_blank"
             >
               Visit
             </a>
             <a
               className="btn-trans  w-[40%]"
-              href="https://www.google.com/search?q=GitHub&rlz"
+              href="https://github.com/abdul-rehan7/Tailwind-CSS/tree/main/microsoft_clone"
               target="_blank"
             >
               GitHub
             </a>
           </div>
         </div>
-        <div className="card space-y-2 md:w-[25%] w-[90vw] h-auto bg-[#1b1b1b] p-2 rounded-sm"   data-aos="zoom-in">
+        <div className="card space-y-2 md:w-[25%] w-[90vw] h-[60vh] bg-[#1b1b1b] p-2 rounded-sm"   data-aos="zoom-in">
           <div className="items space-y-2 ">
-            <img src="/imgs/projs/1.png" alt="" />
-            <h1 className="font-semibold">Archiods</h1>
-            <p className="text-sm font-normal text-center">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt nisi
-              fuga provident. Magni, aperiam! Minima. Lorem ipsum dolor sit amet
-              consectetur adipisicing elit. Velit, omnis!
+            <img src="/imgs/projs/2.png" alt="" />
+            <h1 className="font-semibold">Netflix Clone</h1>
+            <p className="text-sm font-normal text-left h-[17vh]">
+            
+I created a Netflix clone that replicates the streaming service's interface, allowing users to browse, search, and watch videos seamlessly.
             </p>
           </div>
           <div className="buttons flex justify-evenly">
             <a
               className="btn  w-[40%]"
-              href="https://www.google.com/search?q=React&rlz"
+              href="https://ar-netflix.vercel.app/"
               target="_blank"
             >
               Visit
             </a>
             <a
               className="btn-trans  w-[40%]"
-              href="https://www.google.com/search?q=GitHub&rlz"
+              href="https://github.com/abdul-rehan7/Web-dev-projects/tree/main/Netflix%20Clone"
               target="_blank"
             >
               GitHub
             </a>
           </div>
         </div>
-        <div className="card space-y-2 md:w-[25%] w-[90vw] h-auto bg-[#1b1b1b] p-2 rounded-sm"   data-aos="zoom-in">
+        <div className="card space-y-2 md:w-[25%] w-[90vw] h-[60vh] bg-[#1b1b1b] p-2 rounded-sm"   data-aos="zoom-in">
           <div className="items space-y-2 ">
-            <img src="/imgs/projs/1.png" alt="" />
-            <h1 className="font-semibold">Archiods</h1>
-            <p className="text-sm font-normal text-center">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt nisi
-              fuga provident. Magni, aperiam! Minima. Lorem ipsum dolor sit amet
-              consectetur adipisicing elit. Velit, omnis!
+            <img src="/imgs/projs/3.png" alt="" />
+            <h1 className="font-semibold">React Project</h1>
+            <p className="text-sm font-normal text-left h-[17vh]">
+            Created my first React project, replicating the Nike's clean and dynamic interface, showcasing products, and providing an intuitive shopping experience.
             </p>
           </div>
           <div className="buttons flex justify-evenly">
