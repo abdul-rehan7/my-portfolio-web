373e1a18635b07460313279d24fc56619a79833e
rehanmirza76 <abdulrehan0317@gmail.com>
2024-05-31 23:06:34 +0500

    update

--- src/components/TechStack.jsx
+++ src/components/TechStack.jsx
@@ -5,13 +5,13 @@ const TechStack = () => {
     <div className="bg-[#0f0f0f]  items-center h-auto text-center  md:py-5 py-2 space-y-4">
       <h1 className="md:text-4xl text-xl text-[#4d4d4d] font-semibold py-2">TECHNOLOGY STACK</h1>
       <div className="flex stacklist justify-evenly items-center">
-        <i className="bx bxl-html5 md:text-8xl text-4xl" data-aos="fade-up" ></i>
-        <i className="bx bxl-css3 md:text-8xl text-4xl" data-aos="fade-up" ></i>
-        <i className="bx bxl-tailwind-css md:text-8xl text-4xl" data-aos="fade-up" ></i>
-        <i className="bx bxl-javascript md:text-8xl text-4xl" data-aos="fade-up" ></i>
-        <i className="bx bxl-typescript md:text-8xl text-4xl" data-aos="fade-up" ></i>
-        <i className="bx bxl-nodejs md:text-8xl text-4xl" data-aos="fade-up" ></i>
-        <i className="bx bxl-react md:text-8xl text-4xl" data-aos="fade-up" ></i>
+        <i className="bx hover:text-[#bababa] bxl-html5 md:text-8xl text-4xl" data-aos="fade-up" ></i>
+        <i className="bx hover:text-[#bababa] bxl-css3 md:text-8xl text-4xl" data-aos="fade-up" ></i>
+        <i className="bx hover:text-[#bababa] bxl-tailwind-css md:text-8xl text-4xl" data-aos="fade-up" ></i>
+        <i className="bx hover:text-[#bababa] bxl-javascript md:text-8xl text-4xl" data-aos="fade-up" ></i>
+        <i className="bx hover:text-[#bababa] bxl-typescript md:text-8xl text-4xl" data-aos="fade-up" ></i>
+        <i className="bx hover:text-[#bababa] bxl-nodejs md:text-8xl text-4xl" data-aos="fade-up" ></i>
+        <i className="bx hover:text-[#bababa] bxl-react md:text-8xl text-4xl" data-aos="fade-up" ></i>
       </div>
     </div>
   );
